4015256406d6ecfed41f5448ddacf96a0d2401d1
khusamov Albert <khusamov01@mail.ru>
2020-12-03 14:37:01 +0500

    v1.0
    Релаизован класс дерево Tree

---
+++ virtuality/virtuality.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+class Tree {
+private:
+    string class_name = "Tree";
+public:
+    string fruit;
+    Tree() {
+        fruit = "";
+        cout << "Конструктор Tree()";
+    }
+    Tree(string fruit) {
+        this->fruit = fruit;
+        cout << "Конструктор Tree(string fruit)";
+    }
+    Tree(Tree& wood) {
+        fruit = wood.fruit;
+        cout << "Конструктор Tree(Tree& wood)";
+    }
+    virtual string classname() {
+        cout << "Виртуальный метод classname Tree";
+        return class_name;
+    }
+    virtual bool isA(string cn) {
+        if (class_name == cn)
+            return true;
+        else
+            return false;
+    }
+    virtual ~Tree() {
+        cout << "Виртуальный деструктор Tree";
+    }
+};
+
+int main()
+{
+}
