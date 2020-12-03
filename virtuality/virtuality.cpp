01187e5261e1c8ef7b18e047833aff7924d5da5a
khusamov Albert <khusamov01@mail.ru>
2020-12-03 14:51:12 +0500

    v1.1
    Реализованы классы-потомки: Яблоня, Грушевое дерево, Вишневое дерево

--- virtuality/virtuality.cpp
+++ virtuality/virtuality.cpp
@@ -23,6 +23,7 @@ public:
         return class_name;
     }
     virtual bool isA(string cn) {
+        cout << "Виртуальный метод isA Tree";
         if (class_name == cn)
             return true;
         else
@@ -33,6 +34,103 @@ public:
     }
 };
 
+class Apple_Tree: public Tree {
+private:
+    string class_name = "Apple_Tree";
+public:
+    Apple_Tree() {
+        fruit = "Apple";
+        cout << "Конструктор Apple_Tree()";
+    }
+    Apple_Tree(string fruit) {
+        this->fruit = fruit;
+        cout << "Конструктор Apple_Tree(string fruit)";
+    }
+    Apple_Tree(Apple_Tree& wood) {
+        fruit = wood.fruit;
+        cout << "Конструктор Apple_Tree(Tree& wood)";
+    }
+    string classname() {
+        cout << "НеВиртуальный метод classname Apple_Tree";
+        return class_name;
+    }
+    bool isA(string cn) {
+        cout << "НеВиртуальный метод isA Apple_Tree";
+        if (class_name == cn)
+            return true;
+        else
+            return false;
+    }
+    ~Apple_Tree() {
+        cout << "Деструктор Apple_Tree";
+    }
+};
+
+class Pear_Tree : public Tree {
+private:
+    string class_name = "Pear_Tree";
+public:
+    Pear_Tree() {
+        fruit = "Pear";
+        cout << "Конструктор Pear_Tree()";
+    }
+    Pear_Tree(string fruit) {
+        this->fruit = fruit;
+        cout << "Конструктор Pear_Tree(string fruit)";
+    }
+    Pear_Tree(Pear_Tree& wood) {
+        fruit = wood.fruit;
+        cout << "Конструктор Pear_Tree(Tree& wood)";
+    }
+    string classname() {
+        cout << "НеВиртуальный метод classname Pear_Tree";
+        return class_name;
+    }
+    bool isA(string cn) {
+        cout << "НеВиртуальный метод isA Pear_Tree";
+        if (class_name == cn)
+            return true;
+        else
+            return false;
+    }
+    ~Pear_Tree() {
+        cout << "Деструктор Pear_Tree";
+    }
+};
+
+class Cherry_Tree : public Tree {
+private:
+    string class_name = "Cherry_Tree";
+public:
+    Cherry_Tree() {
+        fruit = "Cherry";
+        cout << "Конструктор Cherry_Tree()";
+    }
+    Cherry_Tree(string fruit) {
+        this->fruit = fruit;
+        cout << "Конструктор Cherry_Tree(string fruit)";
+    }
+    Cherry_Tree(Cherry_Tree& wood) {
+        fruit = wood.fruit;
+        cout << "Конструктор Cherry_Tree(Tree& wood)";
+    }
+    string classname() {
+        cout << "НеВиртуальный метод classname Cherry_Tree";
+        return class_name;
+    }
+    bool isA(string cn) {
+        cout << "НеВиртуальный метод isA Cherry_Tree";
+        if (class_name == cn)
+            return true;
+        else
+            return false;
+    }
+    ~Cherry_Tree() {
+        cout << "Деструктор Cherry_Tree";
+    }
+};
+
 int main()
 {
+
 }
